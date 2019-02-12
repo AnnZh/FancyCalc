d2d8723cb3c9b5135bf7bd40a5639bee4d47eaee
AnnZh <ann.zhurok@gmail.com>
2019-02-13 00:58:16 +0300

    Done

--- FancyCalc/FancyCalc.cs
+++ FancyCalc/FancyCalc.cs
@@ -11,28 +11,73 @@ namespace FancyCalc
 
         public double Add(int a, int b)
         {
-            throw new NotImplementedException();
-            //return a + b;
+            double x = a;
+            double y = b;
+            return x + y;
         }
 
 
         public double Subtract(int a, int b)
         {
-            //throw new NotImplementedException();
-            return a - b;
+            double x = a;
+            double y = b;
+            return x - y;
         }
 
 
         public double Multiply(int a, int b)
         {
-            return a * b;
+            double x = a;
+            double y = b;
+            return x * y;
         }
 
         //generic calc method. usage: "10 + 20"  => result 30
         public double Culculate(string expression)
         {
-            throw new NotImplementedException();
+            char[] arr = new char[] { '+', '-', '*', '/' };
+            char str = ' ';
+            double x = 0;
+            double y = 0;
+            double z = 0;
+            string[] words = new string[2];
 
+            foreach (char i in expression)
+            {
+                for(int j = 0; j < arr.Length; j++)
+                {
+                    if (i == arr[j])
+                    {
+                        str = arr[j];
+                        words = expression.Split(new char[] { str });
+                    }
+                }
+            }
+       
+            x = Convert.ToDouble(words[0].Trim());
+            y = Convert.ToDouble(words[1].Trim());
+
+            switch (str)
+            {
+                 case '-':
+                    z = x - y;
+                    break;
+                 case '+':
+                    z = x + y;
+                    break;
+                 case '*':
+                    z = x * y;
+                    break;
+                case '/':
+                    if (y == 0)
+                        throw new DivideByZeroException();
+                    z = x / y;
+                    break;
+                default:
+                     break;
+            }
+            
+               return z;
         }
     }
 }
